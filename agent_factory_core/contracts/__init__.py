55a5e15310fe9312ef52fc0cf80adc649d34811d
Yulia <yb1502@gmail.com>
2026-09-06 14:38:20 +0300

    export runtime audit event contract

--- agent_factory_core/contracts/__init__.py
+++ agent_factory_core/contracts/__init__.py
@@ -4,6 +4,7 @@ from .effective_release_config import EffectiveReleaseConfig
 from .exception_policy import ExceptionPolicy
 from .execution_context import ExecutionContext, build_execution_context
 from .platform_policy import PlatformPolicy
+from .runtime_audit_event import RuntimeAuditEvent
 
 __all__ = [
     "AgentManifest",
@@ -12,5 +13,6 @@ __all__ = [
     "ExceptionPolicy",
     "ExecutionContext",
     "PlatformPolicy",
+    "RuntimeAuditEvent",
     "build_execution_context",
 ]
