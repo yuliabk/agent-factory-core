eb2116b77dde061b2a4afd4f4a836f2b887aed09
Yulia <yb1502@gmail.com>
2026-09-06 14:00:15 +0300

    Export client and effective release contracts

--- agent_factory_core/contracts/__init__.py
+++ agent_factory_core/contracts/__init__.py
@@ -1,3 +1,5 @@
 from .agent_manifest import AgentManifest
+from .client_instance_config import ClientInstanceConfig
+from .effective_release_config import EffectiveReleaseConfig
 
-__all__ = ["AgentManifest"]
+__all__ = ["AgentManifest", "ClientInstanceConfig", "EffectiveReleaseConfig"]
