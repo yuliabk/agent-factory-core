2dba579444e663ab9b805e7922d1b087397ee8b7
Yulia <yb1502@gmail.com>
2026-09-06 14:12:45 +0300

    export policy and ExecutionContext contracts

--- agent_factory_core/contracts/__init__.py
+++ agent_factory_core/contracts/__init__.py
@@ -1,5 +1,16 @@
 from .agent_manifest import AgentManifest
 from .client_instance_config import ClientInstanceConfig
 from .effective_release_config import EffectiveReleaseConfig
+from .exception_policy import ExceptionPolicy
+from .execution_context import ExecutionContext, build_execution_context
+from .platform_policy import PlatformPolicy
 
-__all__ = ["AgentManifest", "ClientInstanceConfig", "EffectiveReleaseConfig"]
+__all__ = [
+    "AgentManifest",
+    "ClientInstanceConfig",
+    "EffectiveReleaseConfig",
+    "ExceptionPolicy",
+    "ExecutionContext",
+    "PlatformPolicy",
+    "build_execution_context",
+]
