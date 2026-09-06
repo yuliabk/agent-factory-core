885dcab1cf78e45a168950890925791ccb90de0c
Yulia <yb1502@gmail.com>
2026-09-06 15:27:58 +0300

    export EvalResult contract

--- agent_factory_core/contracts/__init__.py
+++ agent_factory_core/contracts/__init__.py
@@ -1,6 +1,7 @@
 from .agent_manifest import AgentManifest
 from .client_instance_config import ClientInstanceConfig
 from .effective_release_config import EffectiveReleaseConfig
+from .eval_result import EvalResult
 from .exception_policy import ExceptionPolicy
 from .execution_context import ExecutionContext, build_execution_context
 from .platform_policy import PlatformPolicy
@@ -10,6 +11,7 @@ __all__ = [
     "AgentManifest",
     "ClientInstanceConfig",
     "EffectiveReleaseConfig",
+    "EvalResult",
     "ExceptionPolicy",
     "ExecutionContext",
     "PlatformPolicy",
